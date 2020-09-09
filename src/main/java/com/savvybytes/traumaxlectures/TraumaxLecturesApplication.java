54c8337cf39a798bfca51fe2887db0760249c962
JBoss <abolade.akintomiwa@gmail.com>
2020-09-09 14:54:37 +0100

    Application Setup

---
+++ src/main/java/com/savvybytes/traumaxlectures/TraumaxLecturesApplication.java
@@ -0,0 +1,13 @@
+package com.savvybytes.traumaxlectures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraumaxLecturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraumaxLecturesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/savvybytes/traumaxlectures/TraumaxLecturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savvybytes.traumaxlectures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraumaxLecturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
